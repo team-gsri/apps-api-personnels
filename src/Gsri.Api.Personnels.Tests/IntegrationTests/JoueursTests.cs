ffe4616095d92a2362dbddfb56be3e828bfed8ec
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-05-30 19:56:28 +0200

    refactor: webapi controller integration testing

--- src/Gsri.Api.Personnels.Tests/IntegrationTests/JoueursTests.cs
+++ src/Gsri.Api.Personnels.Tests/IntegrationTests/JoueursTests.cs
@@ -2,7 +2,7 @@ using FluentAssertions;
 using Gsri.Api.Personnels.Database;
 using Gsri.Api.Personnels.Database.Models;
 using Gsri.Api.Personnels.Joueurs.Interfaces;
-using Gsri.Api.Personnels.Tests.Utils.Executors;
+using Gsri.Api.Personnels.Tests.Utils;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Linq.Expressions;
 using System.Net;
@@ -11,18 +11,20 @@ using Xunit;
 
 namespace Gsri.Api.Personnels.Tests.IntegrationTests;
 
-public class JoueursTests : TestExecutorStringBase<Program, PersonnelsDbContext, JoueurEntity>
+public class JoueursTests : ControllerTestBase<Program, PersonnelsDbContext, JoueurEntity>
 {
-    private readonly WebApplicationFactory<Program> factory;
-
-    public JoueursTests(WebApplicationFactory<Program> factory)
-    {
-        this.factory = factory;
-    }
-
     protected override string BaseUrl => "v1/Joueurs";
 
     protected override Expression<Func<JoueurEntity, string>> KeySelector => joueur => joueur.Pseudonyme;
+
+    [Fact]
+    public async Task GetAll()
+    {
+        var factory = new WebApplicationFactory<Program>();
+        var client = factory.CreateClient();
+        var response = await client.GetAsync("v1/Joueurs").ConfigureAwait(false);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
 
     [Theory]
     [InlineData(true, HttpVerbs.Get, HttpStatusCode.OK)]
@@ -33,20 +35,16 @@ public class JoueursTests : TestExecutorStringBase<Program, PersonnelsDbContext,
     [InlineData(false, HttpVerbs.Delete, HttpStatusCode.NotFound)]
     [InlineData(false, HttpVerbs.Post, HttpStatusCode.Created)]
     [InlineData(false, HttpVerbs.Put, HttpStatusCode.MethodNotAllowed)]
-    public async Task JoueursController(bool entity_exists, HttpVerbs method, HttpStatusCode expected)
-    {
-        await Execute(factory, method, entity_exists, expected).ConfigureAwait(false);
-    }
-
-    [Fact]
-    public async Task GetAll()
-    {
-        var client = factory.CreateClient();
-        var response = await client.GetAsync("v1/Joueurs").ConfigureAwait(false);
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-    }
+    public async Task JoueursController(bool entity_exists, HttpVerbs method, HttpStatusCode expected) =>
+        await Execute(method, entity_exists, expected).ConfigureAwait(false);
 
     protected override JsonContent GeneratePostPayload(string key) => JsonContent.Create(new AddJoueurRequest { Pseudonyme = key });
 
     protected override JsonContent GeneratePutPayload(string key) => JsonContent.Create(string.Empty);
+
+    protected override async Task SeedDatabaseAsync(PersonnelsDbContext context)
+    {
+        context.Joueurs.Add(new JoueurEntity { Pseudonyme = "TestUser" });
+        await context.SaveChangesAsync().ConfigureAwait(false);
+    }
 }
---
+++ src/Gsri.Api.Personnels.Tests/Utils/WebApiTestBase.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gsri.Api.Personnels.Tests.Utils;
+
+public abstract class WebApiTestBase<TEntryPoint, TContext> : IDisposable
+    where TEntryPoint : class
+    where TContext : DbContext
+{
+    private readonly WebApplicationFactory<TEntryPoint> factory;
+
+    private readonly IServiceScope scope;
+
+    private readonly IDbContextTransaction transaction;
+
+    private bool disposedValue;
+
+    protected WebApiTestBase()
+    {
+        factory = new WebApplicationFactory<TEntryPoint>();
+        scope = factory.Services.CreateScope();
+        Context = scope.ServiceProvider.GetRequiredService<TContext>();
+        transaction = Context.Database.BeginTransaction();
+        Client = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton(Context);
+            });
+        }).CreateClient();
+    }
+
+    protected HttpClient Client { get; }
+
+    protected TContext Context { get; }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                transaction.Dispose();
+                scope.Dispose();
+            }
+            disposedValue = true;
+        }
+    }
+}
